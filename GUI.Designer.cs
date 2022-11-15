123b85519d266b9c831fecad4a91c47c8f1dd0c1
Daniel Banár <daniel.banar5@gmail.com>
2022-11-15 21:47:51 +0100

    first commit

---
+++ GUI.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace MasinaPC
+{
+	partial class GUI
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.trackBar2 = new System.Windows.Forms.TrackBar();
+			this.trackBar3 = new System.Windows.Forms.TrackBar();
+			this.trackBar4 = new System.Windows.Forms.TrackBar();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(223, 20);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.Text = "192.168.1.67";
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(241, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(67, 20);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "RELOAD";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(86, 592);
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(222, 45);
+			this.trackBar1.TabIndex = 7;
+			this.trackBar1.Value = 10;
+			this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 592);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 13);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "RÝCHLOSŤ:";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(314, 592);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(47, 17);
+			this.checkBox1.TabIndex = 9;
+			this.checkBox1.Text = "LED";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// trackBar2
+			// 
+			this.trackBar2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.trackBar2.Location = new System.Drawing.Point(367, 592);
+			this.trackBar2.Name = "trackBar2";
+			this.trackBar2.Size = new System.Drawing.Size(111, 45);
+			this.trackBar2.TabIndex = 10;
+			this.trackBar2.Value = 10;
+			this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+			// 
+			// trackBar3
+			// 
+			this.trackBar3.BackColor = System.Drawing.Color.Lime;
+			this.trackBar3.Location = new System.Drawing.Point(484, 592);
+			this.trackBar3.Name = "trackBar3";
+			this.trackBar3.Size = new System.Drawing.Size(111, 45);
+			this.trackBar3.TabIndex = 11;
+			this.trackBar3.Value = 10;
+			this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+			// 
+			// trackBar4
+			// 
+			this.trackBar4.BackColor = System.Drawing.Color.Blue;
+			this.trackBar4.Location = new System.Drawing.Point(601, 592);
+			this.trackBar4.Name = "trackBar4";
+			this.trackBar4.Size = new System.Drawing.Size(111, 45);
+			this.trackBar4.TabIndex = 12;
+			this.trackBar4.Value = 10;
+			this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 39);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(972, 547);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 13;
+			this.pictureBox1.TabStop = false;
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 15;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// GUI
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.ClientSize = new System.Drawing.Size(1012, 650);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.trackBar4);
+			this.Controls.Add(this.trackBar3);
+			this.Controls.Add(this.trackBar2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "GUI";
+			this.Text = "Mašina";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.TrackBar trackBar2;
+		private System.Windows.Forms.TrackBar trackBar3;
+		private System.Windows.Forms.TrackBar trackBar4;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ GUI.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Windows.Input;
+namespace MasinaPC
+{
+	public partial class GUI : Form
+	{
+		string ip = "192.168.1.67";
+		HttpClient httpClient;
+		public GUI()
+		{
+			InitializeComponent();
+			this.KeyPreview = true;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			httpClient = new HttpClient();
+			httpClient.Timeout = new TimeSpan(0, 0, 3);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			MjpegProcessor.MjpegDecoder mjpegDecoder = new MjpegProcessor.MjpegDecoder();
+			mjpegDecoder.ParseStream(new Uri($"http://{ip}:5000/stream.mjpg"));
+			mjpegDecoder.FrameReady += (s, e2) =>
+			{
+				pictureBox1.Image = e2.Bitmap;
+			};
+		}
+		private void trackBar1_Scroll(object sender, EventArgs e)
+		{
+			httpClient.GetAsync($"http://{ip}:8000/speed/{(int)((float)trackBar1.Value * 25.5f)}");
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			ip = textBox1.Text;
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked)
+				httpClient.GetAsync($"http://{ip}:8000/led");
+			else
+				httpClient.GetAsync($"http://{ip}:8000/LED");
+		}
+
+		private void trackBar2_Scroll(object sender, EventArgs e)
+		{
+			httpClient.GetAsync($"http://{ip}:8000/r/{(int)((float)trackBar2.Value * 25.5f)}");
+		}
+
+		private void trackBar3_Scroll(object sender, EventArgs e)
+		{
+			httpClient.GetAsync($"http://{ip}:8000/g/{(int)((float)trackBar3.Value * 25.5f)}");
+		}
+
+		private void trackBar4_Scroll(object sender, EventArgs e)
+		{
+			httpClient.GetAsync($"http://{ip}:8000/b/{(int)((float)trackBar4.Value * 25.5f)}");
+		}
+		bool w=false, a=false, s=false, d=false;
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (Keyboard.IsKeyDown(Key.W) && !w)
+			{
+				httpClient.GetAsync($"http://{ip}:8000/w");
+				w = true;
+			}
+			else if (Keyboard.IsKeyUp(Key.W) && w)
+			{
+				httpClient.GetAsync($"http://{ip}:8000/W");
+				w = false;
+			}
+
+			if (Keyboard.IsKeyDown(Key.A) && !a)
+			{
+				httpClient.GetAsync($"http://{ip}:8000/a");
+				a = true;
+			}
+			else if (Keyboard.IsKeyUp(Key.A) && a)
+			{
+				httpClient.GetAsync($"http://{ip}:8000/A");
+				a = false;
+			}
+
+			if (Keyboard.IsKeyDown(Key.S) && !s)
+			{
+				httpClient.GetAsync($"http://{ip}:8000/s");
+				s = true;
+			}
+			else if (Keyboard.IsKeyUp(Key.S) && s)
+			{
+				httpClient.GetAsync($"http://{ip}:8000/S");
+				s = false;
+			}
+
+			if (Keyboard.IsKeyDown(Key.D) && !d)
+			{
+				httpClient.GetAsync($"http://{ip}:8000/d");
+				d = true;
+			}
+			else if (Keyboard.IsKeyUp(Key.D) && d)
+			{
+				httpClient.GetAsync($"http://{ip}:8000/D");
+				d = false;
+			}
+		}
+	}
+}
